ac9426e4fd715fc92f4fd757fba75dfb4728b49d
Max V. <mountainwater101z@gmail.com>
2026-07-28 20:14:49 -0500

    Fix typo in prompt for user input

--- story_practice.py
+++ story_practice.py
@@ -2,7 +2,7 @@ print("xo" * 50)
 print("      Red Hood Fanfic       ")
 print("xo" * 50)
 
-print("\n WHAT WOULD YOU LIKE TO BE REFERRED AS?)
+print("\n WHAT WOULD YOU LIKE TO BE REFERRED AS?")
 name = input("Enter your name: ").strip()
 if name == "":
   name = "Traveler"
